22832c6968e660ccf0d8be2e310f260c3f8534e0
starling2022 <nguyenhoanglong6d@gmail.com>
2024-03-13 22:42:04 +0700

    upt bedao contest

---
+++ THPT_HaiPhong_2023/hp_thpt_23_d.cpp
@@ -0,0 +1,3 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i,a,b) for(int i = (a); i <= (b); ++i)
---
+++ simple/scratch_t01_parity.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define int long long
+#define FOR(i, a, b) for(int i=(a); i<=(b);++i)
+#define FOD(i, a, b) for(int i=(a); i>=(b);--i)
+#define REP(n) for(int i=0; i<=(n);++i)
+#define FAST ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define FILE_IO(filein, fileout) freopen((filein), "r", stdin);freopen((fileout), "w", stdout);
+using namespace std;
+const int N=10000007;
+
+int a;
+
+int32_t main(void){
+    cin >> a;
+    if (a%2==0) cout << "YES";
+    else cout << "NO";
+}
